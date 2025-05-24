39eca599c34c3cae6aa41c207aae490e52a47bc0
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-05-24 17:33:29 +0000

    .

--- main/AI.py
+++ main/AI.py
@@ -66,3 +66,14 @@ Reply in markdown format
 
 {example_text}
 """
+
+qwen = Model("Qwen", "qwen2.5:latest","main/saves/context.json", SYSTEM_PROMPT, True, False)
+deepseek_R1 = Model("R1","deepseek-r1:latest","main/saves/context.json",SYSTEM_PROMPT, False, True)
+
+MODELS = [qwen, deepseek_R1]
+
+async def warm_up():
+    for m in MODELS:
+        await m.warm_up(True)
+
+asyncio.run(warm_up())
