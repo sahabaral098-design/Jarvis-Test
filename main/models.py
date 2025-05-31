97b02c44da9af8c7669fbaaef7b2d04050d47898
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-05-31 08:43:20 +0000

    .

--- main/models.py
+++ main/models.py
@@ -1,30 +1,47 @@
-import requests
+import aiohttp
 import json
-import subprocess
 
 class Model:
-    def __init__(self, models_config_path = "main/Models_config.json") -> None:
-        # ---- Models config loading ----
+    def __init__(self,role:str, name:str, ollama_name:str, has_tools:bool, has_CoT:bool, port:int, system_prompt:str) -> None:
+        self.role = role
+        self.name = name
+        self.ollama_name = ollama_name
+        self.has_tools = has_tools
+        self.has_CoT = has_CoT
+        self.port = port
+        self.system = system_prompt
 
-        try:
-            with open(models_config_path, "r") as f:
-                models_config:dict = json.load(f) 
-                if models_config: # To see if it even exists or just is there to make me scream
-                    models_config = models_config.get("Models")  # type: ignore : pylance
-                else:
-                    print("`Models` configs not found")
-                    exit(1)
-                print(models_config)
-        except (json.JSONDecodeError, Exception) as e:
-            print(f"An error occured loading the Models config file: {e}")
-            exit(1)
+        self.host = f"http://localhost:{self.port}"
 
-        # ---- Ollama setup ----
-
-        subprocess.Popen(["ollama", "serve"],  stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-
-        for model in models_config: print(model)
+        self.start_command = f"OLLAMA_HOST={self.host} ollama serve"
         
+        self.warmed_up = False
 
-if __name__ == "__main__": # TEST
-    test = Model()
+    async def generate_response(self, query:str):
+        url = f"{self.host}/api/chat" # For API calling
+        headers = {"Content-Type": "application/json"}
+        data = {
+            "model": self.ollama_name,
+            "messages": [{"role": "system", "content": self.system},{"role": "user", "content": query}],
+            "stream": False,
+        }
+        if not self.warmed_up:
+            print(f"{self.name}({self.ollama_name}) warming up...") # To prevent Loading lag...
+            query = "hi"
+            data = {
+            "model": self.ollama_name,
+            "messages": [{"role": "system", "content": self.system},{"role": "user", "content": query}],
+            "stream": False,
+            }
+            
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, data=json.dumps(data)) as response:
+                    response.raise_for_status()
+                    return await response.json()
+        else:
+            print("Generating response...")
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, data=json.dumps(data)) as response:
+                    response.raise_for_status()
+                    return await response.json()
+
