58c14b4f913e46d942d6ed93318a2d206fc232fc
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-07-26 16:25:34 +0530

    Update Text2Speech.py

--- main/Voice/Text2Speech.py
+++ main/Voice/Text2Speech.py
@@ -7,3 +7,12 @@ text = "I might not be jutstice but. I. AM. INSANE!"
 out = "output.wav"
 
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+
+tts.save_to_file(
+  text = text,
+  speaker_wav = speak_sample,
+  language= "en",
+  file_path= out,
+)
+
+print("done")
