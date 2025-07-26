2e92226715b53ab4d8995c7113edb64f537e6780
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-07-26 16:38:29 +0530

    Update Text2Speech.py

--- main/Voice/Text2Speech.py
+++ main/Voice/Text2Speech.py
@@ -1,18 +1,17 @@
+import os
 from TTS.api import TTS
 
-# Test
-
-speak_sample= "/assets/Kira.wav"
-text = "I might not be jutstice but. I. AM. INSANE!"
+speak_sample = "./assets/Kira.wav"
+text = "I might not be justice... but I. AM. INSANE!"
 out = "output.wav"
 
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
 
 tts.tts_to_file(
-  text = text,
-  speaker_wav = speak_sample,
-  language= "en",
-  file_path= out,
+    text=text,
+    speaker_wav=speak_sample,
+    language="en",
+    file_path=out
 )
 
-print("done")
+print(f"✅ Done! File saved to: {os.path.abspath(out)}")
