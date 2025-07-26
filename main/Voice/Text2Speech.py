5eb01cebd238cae5e001f64063933876da15f5b6
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-07-26 10:42:04 +0000

    idk

--- main/Voice/Text2Speech.py
+++ main/Voice/Text2Speech.py
@@ -2,3 +2,8 @@ from TTS.api import TTS
 
 # Test
 
+speak_sample= "/assets/Kira.wav"
+text = "I might not be jutstice but. I. AM. INSANE!"
+out = "output.wav"
+
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
