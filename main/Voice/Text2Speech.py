eb85a5b50524e2e75469d76d904cf8978627d125
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-07-26 11:27:23 +0000

    idk

--- main/Voice/Text2Speech.py
+++ main/Voice/Text2Speech.py
@@ -1,13 +1,10 @@
 from TTS.api import TTS
-import torch
-
-gpu= torch.cuda.is_available()
 
 speak_sample = "./assets/Kira.wav"
 text = "I might not be justice... but I. AM. INSANE!"
 out = "output.wav"
 
-tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=gpu)
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2").to("cpu")
 tts.tts_to_file(
     text=text,
     speaker_wav=speak_sample,
